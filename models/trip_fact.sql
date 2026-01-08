94b329b2dc213b2ddebb8c2e4ce10f14706224f5
iandarakis@hotmail.com <iandarakis@hotmail.com>
2026-01-08 22:38:28 +0000

    create trip_fact

---
+++ models/trip_fact.sql
@@ -0,0 +1,15 @@
+WITH trips  AS
+(
+    SELECT ride_id
+    , rideable_type
+    , TRY_TO_TIMESTAMP(started_at)::DATE trip_date
+    , start_station_id
+    , end_station_id
+    , member_casual
+    , TIMESTAMPDIFF(SECONDS, TRY_TO_TIMESTAMP(started_at), TRY_TO_TIMESTAMP(ended_at)) trip_duration_seconds
+    FROM {{ source('demo', 'bike') }}
+    WHERE TRY_TO_TIMESTAMP(started_at) IS NOT NULL
+)
+
+SELECT *
+FROM trips
